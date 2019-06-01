ba8abb85eee02862d1f773ed9f24b02cfea2ad87
echo <897960700@qq.com>
2019-06-02 01:06:31 +0800

    all

--- src/Route.php
+++ src/Route.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Echo;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 /**
  *
@@ -48,10 +48,124 @@ class Route {
     self::$halts = $flag;
   }
 
+   public static function dispatch(){
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $searches = array_keys(static::$patterns);
+    $replaces = array_values(static::$patterns);
+
+    $found_route = false;
+
+    self::$routes = preg_replace('/\/+/', '/', self::$routes);
+
+    // Check if route is defined without regex
+    if (in_array($uri, self::$routes)) {
+      $route_pos = array_keys(self::$routes, $uri);
+      foreach ($route_pos as $route) {
+
+        // Using an ANY option to match both GET and POST requests
+        if (self::$methods[$route] == $method || self::$methods[$route] == 'ANY' || (!empty(self::$maps[$route]) && in_array($method, self::$maps[$route]))) {
+          $found_route = true;
+
+          // If route is not an object
+          if (!is_object(self::$callbacks[$route])) {
+
+            // Grab all parts based on a / separator
+            $parts = explode('/',self::$callbacks[$route]);
+
+            // Collect the last index of the array
+            $last = end($parts);
+
+            // Grab the controller name and method call
+            $segments = explode('@',$last);
+
+            // Instanitate controller
+            $controller = new $segments[0]();
+
+            // Call method
+            $controller->{$segments[1]}();
+
+            if (self::$halts) return;
+          } else {
+            // Call closure
+            call_user_func(self::$callbacks[$route]);
+
+            if (self::$halts) return;
+          }
+        }
+      }
+    } else {
+      // Check if defined with regex
+      $pos = 0;
+      foreach (self::$routes as $route) {
+        if (strpos($route, ':') !== false) {
+          $route = str_replace($searches, $replaces, $route);
+        }
+
+        if (preg_match('#^' . $route . '$#', $uri, $matched)) {
+          if (self::$methods[$pos] == $method || self::$methods[$pos] == 'ANY' || (!empty(self::$maps[$pos]) && in_array($method, self::$maps[$pos]))) {
+            $found_route = true;
+
+            // Remove $matched[0] as [1] is the first parameter.
+            array_shift($matched);
+
+            if (!is_object(self::$callbacks[$pos])) {
+
+              // Grab all parts based on a / separator
+              $parts = explode('/',self::$callbacks[$pos]);
+
+              // Collect the last index of the array
+              $last = end($parts);
+
+              // Grab the controller name and method call
+              $segments = explode('@',$last);
+
+              // Instanitate controller
+              $controller = new $segments[0]();
+
+              // Fix multi parameters
+              if (!method_exists($controller, $segments[1])) {
+                echo "controller and action not found";
+              } else {
+                call_user_func_array(array($controller, $segments[1]), $matched);
+              }
+
+              if (self::$halts) return;
+            } else {
+              call_user_func_array(self::$callbacks[$pos], $matched);
+
+              if (self::$halts) return;
+            }
+          }
+        }
+        $pos++;
+      }
+    }
+
+    // Run the error callback if the route was not found
+    if ($found_route == false) {
+      if (!self::$error_callback) {
+        self::$error_callback = function() {
+          header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
+          echo '404';
+        };
+      } else {
+        if (is_string(self::$error_callback)) {
+          self::get($_SERVER['REQUEST_URI'], self::$error_callback);
+          self::$error_callback = null;
+          self::dispatch();
+          return ;
+        }
+      }
+      call_user_func(self::$error_callback);
+    }
+  }
+
   /**
    * Runs the callback for the given request
    */
-  public static function dispatch(){
+  /*public static function dispatch(){
     $request = Request::createFromGlobals();
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $method = $_SERVER['REQUEST_METHOD'];
@@ -171,5 +285,5 @@ class Route {
       }
       call_user_func(self::$error_callback);
     }
-  }
+  }*/
 }
