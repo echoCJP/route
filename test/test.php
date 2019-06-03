d007da2338ecc1500ca8228de4332109e5c857bf
echo <897960700@qq.com>
2019-06-03 23:45:26 +0800

    Yob路由v1.0

--- test/test.php
+++ test/test.php
@@ -1,11 +1,8 @@
 <?php
 
 use Yob\Route;
-// use Evai\Tenden\Route;
-// use Echoo\Route;
 
 $route = new Route();
-// var_dump($route);exit;
 Route::get('/', function() {
   echo "Welcome";
 });
@@ -14,15 +11,11 @@ Route::get('/name/(:all)', function($name) {
   echo 'Your name is '.$name;
 });
 
-Route::get('home', 'HomeController@home');
-Route::get('mail', 'HomeController@mail');
-Route::get('redis', 'HomeController@redis');
-Route::get('test', 'HomeController@test');
+Route::get('test', 'IndexController@test');
 
 Route::error(function() {
     //throw new Exception("404 Not Found");
     echo '404 Not Found';
 });
-
 
 Route::dispatch();
